2710913aa8f25d5ec6ee8313b3e101ca67bb692f
SaranHasnor <SaranHasnor@users.noreply.github.com>
2014-01-05 23:15:19 +0100

    Début de travail sur les spawners

---
+++ FightForMe/Assets/Scripts/Behaviour/MonsterCamp/MonsterCampScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterCampScript : MonoBehaviour
+{
+	private MonsterSpawnerScript[] _spawners;	// Spawners that are part of this camp
+
+	private uint currentLevel;					// Current level of the camp (goes up each time all the monsters die)
+	private uint stillAlive;					// Monsters still alive
+
+	void Start()
+	{
+		_spawners = this.GetComponentsInChildren<MonsterSpawnerScript>();
+
+		if (_spawners.Length == 0)
+		{ // No reason to be here...
+			Destroy(this.gameObject);
+			return;
+		}
+
+		for (int i = 0; i < _spawners.Length; i++)
+		{
+			_spawners[i].LinkToCamp(this);
+		}
+
+		currentLevel = 0;
+		stillAlive = 0;
+	}
+
+	private void Spawn()
+	{
+		for (int i = 0; i < _spawners.Length; i++)
+		{
+			_spawners[i].Spawn();
+			stillAlive++;
+		}
+	}
+
+	public void TrySpawn()
+	{
+		if (stillAlive == 0)
+		{
+			Spawn();
+		}
+	}
+
+	public uint GetLevel()
+	{
+		return this.currentLevel;
+	}
+
+	public void OnBoundMonsterDeath()
+	{
+		if (stillAlive <= 0)
+		{
+			Debug.Log("ERROR: Camp was not expecting any more death events");
+			return;
+		}
+
+		stillAlive--;
+		if (stillAlive == 0)
+		{
+			currentLevel++;
+		}
+	}
+}
---
+++ FightForMe/Assets/Scripts/Behaviour/MonsterSpawner/MonsterSpawnerScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterSpawnerScript : MonoBehaviour
+{
+	[SerializeField] // WHY CAN'T UNSIGNED INTEGERS BE SERIALIZED
+	private int[] _monsterList;		// List of monsters we should spawn (index is the level of the camp)
+
+	private MonsterCampScript camp;	// Camp we're a part of
+
+	public void LinkToCamp(MonsterCampScript camp)
+	{
+		this.camp = camp;
+	}
+
+	void Start()
+	{
+		if (!camp)
+		{ // We're not linked to a trigger, we're probably just a one-time spawner then
+			Spawn();
+			Destroy(this.gameObject);
+			return;
+		}
+
+		if (_monsterList.Length == 0)
+		{ // We don't have any bound monsters, no reason for us to be here
+			Destroy(this.gameObject);
+			return;
+		}
+	}
+
+	public void Spawn()
+	{
+		if (_monsterList.Length == 0)
+		{ // We don't have any bound monsters, no reason for us to be here
+			Destroy(this.gameObject);
+			return;
+		}
+
+		uint monsterID;
+
+		if (!camp)
+		{ // Just spawn our first entry
+			 monsterID = (uint)_monsterList[0];
+		}
+		else
+		{ // Check the level of the camp, and spawn our entry with that index
+			if (_monsterList.Length < camp.GetLevel() + 1)
+			{
+				monsterID = (uint)_monsterList[_monsterList.Length-1];
+			}
+			else
+			{
+				monsterID = (uint)_monsterList[camp.GetLevel()];
+			}
+		}
+
+		Monster monster = DataTables.getMonster(monsterID);
+
+		// TODO: Spawn him
+	}
+}
