f01c45975860d96f0e666d2f6afedf063c1e18ca
SaranHasnor <SaranHasnor@users.noreply.github.com>
2014-01-11 14:47:09 +0100

    Début du travail sur l'inventaire

--- FightForMe/Assets/Scripts/Behaviour/Character/CharacterInventoryScript.cs
+++ FightForMe/Assets/Scripts/Behaviour/Character/CharacterInventoryScript.cs
@@ -4,4 +4,21 @@ using System.Collections;
 public class CharacterInventoryScript : MonoBehaviour
 {
 	public ArrayList items;
+
+	public void DropItem(uint index)
+	{
+
+	}
+
+	public uint PickUpItem(uint item)
+	{
+		uint conflictingItem = 0; // If we have a weapon currently equipped on that same slot, return it
+
+		// Here: foreach in items, check the slot of each item and find one conflicting
+		// Once we find it, set conflictingItem to its index value
+
+		this.items.Add(item);
+
+		return conflictingItem;
+	}
 }
---
+++ FightForMe/Assets/Scripts/Behaviour/DroppedItem/DroppedItemScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroppedItemScript : MonoBehaviour
+{
+	private uint itemID;	// Index of the entry in the item table this entity represents
+
+	public uint GetItemID()
+	{
+		return itemID;
+	}
+
+	// Add an "OnClick()" function that displays a menu allowing the user to pick up or recycle the item
+	public void OnPickUp(CharacterInventoryScript characterInventory)
+	{
+		uint conflictingItem = characterInventory.PickUpItem(this.itemID);
+
+		if (conflictingItem == 0)
+		{ // No conflict found, he simply took our item, we have no reason to be here anymore
+			Destroy(this.gameObject);
+			return;
+		}
+
+		this.itemID = conflictingItem;
+	}
+
+}
