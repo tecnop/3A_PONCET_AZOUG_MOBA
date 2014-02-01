da7fc9cbc45d49552ede91e01880acbbd90d44b6
SaranHasnor <SaranHasnor@users.noreply.github.com>
2014-02-01 17:40:06 +0100

    Fichiers manquant dans le commit précédent

---
+++ FightForMe/Assets/Scripts/Behaviour/Misc/AreaOfEffectScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AreaOfEffectScript
+{
+	//[SerializeField]
+	private static GameObject damageSpherePrefab;
+
+	public static void DoAoE()	// No, I'm not changing it
+	{
+		// Can't call Instantiate from a non-MonoBehaviour class...
+	}
+}
---
+++ FightForMe/Assets/Scripts/Behaviour/Misc/DetectionSphereScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectionSphereScript : MonoBehaviour
+{
+	private ArrayList entities;
+
+	void Start()
+	{
+		entities = new ArrayList();
+		Debug.Log("Spawned");
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		entities.Add(col);
+		Debug.Log("Collision");
+	}
+
+	public ArrayList GetResults()
+	{ // Return our results and destroy us
+		Debug.Log("Killed");
+		Destroy(this.gameObject);
+		return this.entities;
+	}
+}
